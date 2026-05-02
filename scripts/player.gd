cc4b417a7ca61ac4f9290c1ce0a6402d10e8fa0e
moaazkamel <moaaz.kamel.gber@gmail.com>
2026-05-02 18:01:19 +0300

    change the position of the platforms and making the code of the player run an jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,59 @@
 extends CharacterBody2D
 
 
-const SPEED = 95.0
-const JUMP_VELOCITY = -300.0
+var speed = 95.0
+
+var jump_velocity = -285.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
+var coyote_time = 0.15 
+
+var coyote_timer = 0.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		velocity.y += gravity * delta
+		coyote_timer -= delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		coyote_timer = coyote_time
+	if Input.is_action_just_pressed(' jump') and coyote_timer > 0:
+		velocity.y = jump_velocity
+		coyote_timer = 0
+
+
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction	 > 0:
+		animated_sprite.flip_h = false 
+	elif direction < 0:
+		animated_sprite.flip_h = true 
+	
+	
+	if is_on_floor():
+		
+	
+		if direction == 0:
+			animated_sprite.play("idel")
+		else:
+		
+			animated_sprite.play("run")
+	
+	else: 
+		animated_sprite.play("jump")
+	
+	if direction:
+		
+		
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	
+	
